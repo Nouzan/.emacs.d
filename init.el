9f603b1946c57cdc3d6843b9153284086b24fa26
Nouzan <linouzan@gmail.com>
2018-11-13 15:28:02 +0800

    add some config

--- init.el
+++ init.el
@@ -1,1 +1,17 @@
+;;use wombat theme
 (load-theme 'wombat t)
+
+;;coding-system
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+;;MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;;org
+(global-set-key "\C-cc" 'org-capture)
+
